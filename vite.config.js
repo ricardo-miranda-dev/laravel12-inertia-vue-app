5e4f8dbcdea751af3415b3371d2b5dcecac7775d
ricardo-miranda-dev <rmiranda@datafact.com.ec>
2026-03-18 16:26:50 -0500

    feat: implement authentication scaffolding using Laravel Breeze with Vue 3 and Inertia.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,33 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        vue(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
         },
     },
-});
+    server: {
+        host: true, // hace que 0.0.0.0 se use automáticamente
+        port: 5173,
+        strictPort: true,
+		watch: {
+        usePolling: true,
+        interval: 100, // milisegundos
+		},
+        hmr: {
+            protocol: 'ws',   // WebSocket
+            host: 'localhost', // o la IP de tu host si es necesario
+            port: 5173,
+        },
+    },
+})
