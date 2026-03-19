57c2583c9b1ca10a770e43e17bb1eb97d4802380
ricardo-miranda-dev <rmiranda@datafact.com.ec>
2026-03-19 17:34:15 -0500

    feat: implement student create flow with form validation and API-driven dependent dropdowns

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreStudentRequest;
+use App\Http\Resources\ClassesResource;
 use App\Http\Resources\StudentResource;
+use App\Models\Classes;
 use App\Models\Student;
 use Illuminate\Http\Request;
 
@@ -13,5 +16,20 @@ class StudentController extends Controller
         $students = StudentResource::collection (Student::paginate(10));
         
         return inertia('Students/Index', compact('students'));
+    }
+
+    public function create() 
+    {
+       
+        $classes = ClassesResource::collection(Classes::all());
+        return inertia('Students/Create', compact('classes'));
+    }
+
+    public function store(StoreStudentRequest $request ) 
+    {
+       
+        Student::create($request->validated());
+
+        return redirect()->route('students.index');
     }
 }
