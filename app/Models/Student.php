6b6b1d7fae4be59a9e5decbf72a99f1f205d7424
ricardo-miranda-dev <rmiranda@datafact.com.ec>
2026-03-19 11:34:30 -0500

    feat: implement core data layer including models, migrations, factories, seeders, and relationships

---
+++ app/Models/Student.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Student extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = ['name','class_id','section_id','email'];
+    
+    protected $with = ['class','section'];
+
+
+    public function class() 
+    {
+        return $this->belongsTo(Classes::class,'class_id');
+    }
+    
+    public function section() 
+    {
+        return $this->belongsTo(Section::class,'section_id');
+    }
+}
